d65f333688b2b6abaed23094c29649b5c4174e0f
radhika0811 <radhika.agrawal_cs18@gla.ac.in>
2020-03-27 23:28:28 +0530

    add my class MyMain.java

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,20 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("tushar", 1);
+        obj.insert("radhika", 4);
+        obj.insert("yash", 3);
+        obj.insert("shashank", 2);
+        obj.insert("deepika", 5);
+        obj.display();
+    }
 }
+
